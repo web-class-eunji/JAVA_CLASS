47c4705276f02931bc73c47f29d4c43df7b3ac27
eunji <moeunji12@gmail.com>
2025-02-04 16:49:27 +0900

    day1

---
+++ java/src/day1/_02_EscapeSequnce.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class _02_EscapeSequnce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Writ\n\n\nonce!"); //줄바꿈
+		System.out.println("사과\t배\t오렌지"); // 탭
+//		System.out.println("apple\tpearpear\torange");
+		System.out.println("우리집 강쥐는 \"멍멍\" 짖어요"); // 큰따옴표 문자열처리
+		
+		// 한줄 주석입니다.
+		
+		/*
+		 * 여러줄 주석입니다.
+		 *  
+		 */
+	}
+
+}
---
+++ java/src/day1/_03_print.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class _03_print {
+
+	public static void main(String[] args) {
+		System.out.println("apple");
+		System.out.println("banana");
+		System.out.println("orange");
+		// 문자열을 출력하므로 큰따옴표 사용 : 두개 이상의 문자 출력
+		
+		System.out.println("");
+		System.out.println(5);
+		System.out.println(3.14);
+		System.out.println('P');
+		
+		System.out.println("");
+		
+		System.out.println(3+5);
+		System.out.println(3.9+5.72);
+		System.out.println(3.9*5.72);
+		System.out.println(10/3);
+		// 정수 나눗셈의 결과는 몫을 나타낸다.
+		System.out.println(10/3.0);
+		//피연산중 실수가 하나라도 있으면 결과값은 실수로 출력된다.
+	}
+
+}
